e2cb22fb84efdd21fff221eb639c9496c5369537
Mateo Molina <robert.mateo.3012@gmail.com>
2026-05-09 19:55:29 -0500

    feat: primer commit

---
+++ src/main/java/com/krakedev/examen_mateo_molina/ExamenMateoMolinaApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_mateo_molina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMateoMolinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMateoMolinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakedev/examen_mateo_molina/ExamenMateoMolinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_mateo_molina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMateoMolinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
