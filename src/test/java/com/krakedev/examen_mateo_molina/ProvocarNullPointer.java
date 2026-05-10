d71dc9948e68c65572b5fc96a9257a60bbcaba69
Mateo Molina <robert.mateo.3012@gmail.com>
2026-05-09 20:53:16 -0500

    feat: se realizo la parte 3, la cual va enfocado al NullPointerException, el cual se realizo un video y se mostro el error y corrigio

---
+++ src/test/java/com/krakedev/examen_mateo_molina/ProvocarNullPointer.java
@@ -0,0 +1,17 @@
+package com.krakedev.examen_mateo_molina;
+
+import com.krakedev.examen_mateo_molina.entidades.Vendedor;
+import com.krakedev.examen_mateo_molina.servicios.AdminVentas;
+
+public class ProvocarNullPointer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		AdminVentas admin = new AdminVentas();
+		Vendedor v = new Vendedor("015056");
+		admin.agregar(v);
+	}
+
+}
